2f6782cfec0de236dc83db7588668dd31aa61195
Dominik Widmann <dominik.widmann@live.de>
2021-12-17 07:53:11 +0100

    Update launcher text

--- launcher.py
+++ launcher.py
@@ -4,9 +4,10 @@ from main import run_bot
 if __name__ == '__main__':
     # get user inputs
     print(
-        "Hello there!:)\nI'm the impfboosterbot. To be able to book a vaccination appointment for you, please help me out "
+        "\nHello there!:)\nI'm the impfboosterbot. To be able to book a vaccination appointment for you, please help me out "
         "with the necessary account details of your account at the Bayrisches Impfzentrum. Don't worry, I won't use it "
-        "for anything other than booking an appointment for you. I promise! \n")
+        "for anything other than booking an appointment for you. I promise! Please make sure you type everything "
+        "correctly. To submit your input, press ENTER.\n")
 
     # username
     username = input("Username (e.g. santa@claus.com): ")
@@ -23,6 +24,8 @@ if __name__ == '__main__':
 
     # latest_date
     latest_date = input("Please enter the latest date that works for you in the format 'YYYY-MM-DD': ")
+
+    print("Thank you. Let me get you an appointment...")
 
     # start the bot
     run_bot(username, password, person_number, earliest_date, latest_date, iscompiled=True)
